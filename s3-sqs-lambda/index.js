ded147bf3452e93319c423c49f97f167343520e8
Saurya Dhwoj Acharya <saurya.dacharya@gmail.com>
2025-07-22 18:32:13 +1000

    added missing semicolon

--- s3-sqs-lambda/index.js
+++ s3-sqs-lambda/index.js
@@ -7,13 +7,13 @@ exports.handler = async (event) => {
     const batchItemFailures = [];
 
     for (const record of event.Records) {
-        const messageId = record.messageId
+        const messageId = record.messageId;
         try {
             const messageBody = JSON.parse(record.body);
 
             for (s3Record of messageBody.Records) {
                 const bucket = s3Record.s3.bucket.name;
-                const key = s3Record.s3.object.key
+                const key = s3Record.s3.object.key;
 
                 const response = await s3Client.send(new GetObjectCommand({ Bucket: bucket, Key: key }));
                 const fileContent = await response.Body?.transformToString();
