6dc97761091d10c230b8e1584c902a3dcc8c65a0
Saurya Dhwoj Acharya <saurya.dacharya@gmail.com>
2025-07-22 18:29:49 +1000

    added dlq, batch item failures, s3 read

--- s3-sqs-lambda/index.js
+++ s3-sqs-lambda/index.js
@@ -1,14 +1,29 @@
+const {S3Client, GetObjectCommand} = require("@aws-sdk/client-s3");
+
+const s3Client = new S3Client();
+
 exports.handler = async (event) => {
     console.log("Event received:", JSON.stringify(event));
+    const batchItemFailures = [];
 
     for (const record of event.Records) {
-        const messageBody = JSON.parse(record.body);
+        const messageId = record.messageId
+        try {
+            const messageBody = JSON.parse(record.body);
 
-        for (s3Record of messageBody.Records) {
-            const bucket = s3Record.s3.bucket.name;
-            const key = s3Record.s3.object.key
+            for (s3Record of messageBody.Records) {
+                const bucket = s3Record.s3.bucket.name;
+                const key = s3Record.s3.object.key
 
-            console.log(`New file uploaded: Bucket=${bucket}, Key=${key}`);
+                const response = await s3Client.send(new GetObjectCommand({ Bucket: bucket, Key: key }));
+                const fileContent = await response.Body?.transformToString();
+                console.log(JSON.parse(fileContent));
+            }
+        } catch (e) {
+            console.error(`Error processing SQS message ${messageId}`, e);
+            batchItemFailures.push({ itemIdentifier: messageId });
         }
     }
+
+    return { batchItemFailures };
 };
